4f612c95444c95d3a490f2e8de820a9bbbe092c5
AseemAngiras <aseem.angiras@gmail.com>
2026-03-01 11:16:41 +0530

    env added

--- client/src/rtkServices/index.ts
+++ client/src/rtkServices/index.ts
@@ -1,8 +1,9 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
+const BASE_URL = import.meta.env.VITE_BASE_URL
 export const baseApi = createApi({
   reducerPath: 'baseApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://otsserver.ddns.net/api' }),
+  baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
   tagTypes: ['Layouts', 'Layout'],
   endpoints: () => ({})
 })
