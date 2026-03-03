d5272bb876d87dc73c83ca411d57b432403a6a9f
AseemAngiras <aseem.angiras@gmail.com>
2026-03-03 16:59:56 +0530

    Fix: Env

--- client/src/rtkServices/index.ts
+++ client/src/rtkServices/index.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-const BASE_URL = import.meta.env.VITE_BASE_URL
+const BASE_URL = import.meta.env.VITE_APP_BASE_URL
 export const baseApi = createApi({
   reducerPath: 'baseApi',
   baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
